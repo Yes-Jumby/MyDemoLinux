37c50cf0a3a963a143681d30f4c8724113d1b4ac
Yes-Jumby <yangwangpo@126.com>
2020-12-23 13:56:23 +0800

    hello

--- MakeDirTest/main.cpp
+++ MakeDirTest/main.cpp
@@ -4,8 +4,12 @@
 using namespace std;
 int main()
 {
-    string str = "/home/innovation-2/imu/hello1";
-    int a = mkdir(str.c_str(),S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);//same as use mkdir in terminal
-    cout<<a;
+    string str1 = "/home/innovation-2/imu/hello1";
+
+    string str2 = "../../hello1/hello1";
+    int a = mkdir(str1.c_str(),S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);//same as use mkdir in terminal
+    cout<<a<<endl;
+    a = mkdir(str2.c_str(),S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);//same as use mkdir in terminal
+    cout<<a<<endl;
     return 0;
 }
